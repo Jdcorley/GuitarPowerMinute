6ada0ac86a3aec455392f453784834eeb9627f1f
Jack Corley <jdcorley@gmail.com>
2020-11-04 16:04:32 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,30 @@
+def clear_log
+  system("clear")
+end
+
+def restart
+  clear_log
+  system("ruby main.rb")
+end
+
+def call_note
+  cs  = %w[A A# B C C# D D# E F F# G G#]
+  puts cs.sample
+  sleep 1
+  puts "..."
+  sleep 1
+  clear_log
+end
+
+def one_minute
+  30.times do
+    call_note
+  end
+end
+
+def run
+  one_minute
+  restart
+end
+
+run(&one_minute)
